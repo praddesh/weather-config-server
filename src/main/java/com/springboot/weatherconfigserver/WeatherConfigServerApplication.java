f2d93e167c75b19f7ec4911f9f287b89b587936c
praddesh <pradeep.acky@gmail.com>
2023-08-28 20:26:43 +0530

    initial code changes

---
+++ src/main/java/com/springboot/weatherconfigserver/WeatherConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.weatherconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherConfigServerApplication.class, args);
+	}
+
+}
